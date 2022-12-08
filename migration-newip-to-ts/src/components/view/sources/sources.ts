be239fc2724839eb7dd8cc2269a8f0c10ee3e5cd
Alexandr Kabanau <Azraill1989@gmail.com>
2022-12-08 21:57:09 +0300

    fix: fix sources-class

---
+++ migration-newip-to-ts/src/components/view/sources/sources.ts
@@ -0,0 +1,28 @@
+import './sources.css';
+
+export interface ISourcesData {
+    id: string;
+    name: string;
+}
+class Sources {
+    draw(data: Array<ISourcesData>): void {
+        const fragment: DocumentFragment = document.createDocumentFragment();
+        const sourceItemTemp: HTMLTemplateElement = document.querySelector('#sourceItemTemp') as HTMLTemplateElement;
+
+        data.forEach((item) => {
+            const sourceClone = sourceItemTemp.content.cloneNode(true) as HTMLTemplateElement;
+
+            const tempSourceItemName = sourceClone.querySelector('.source__item-name') as HTMLElement;
+            tempSourceItemName.textContent = item.name;
+            const tempSourceItem = sourceClone.querySelector('.source__item') as HTMLElement;
+            tempSourceItem.setAttribute('data-source-id', item.id);
+
+            fragment.append(sourceClone);
+        });
+
+        const tempSources = document.querySelector('.sources') as HTMLElement;
+        tempSources.append(fragment);
+    }
+}
+
+export default Sources;
