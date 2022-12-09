6372172668653d7756009284d23b6daf5008a8ed
AlexKabanau <azraill1989@gmail.com>
2022-12-09 15:48:23 +0300

    feat: app realization - in process

---
+++ migration-newip-to-ts/src/components/app/app.ts
@@ -0,0 +1,22 @@
+import AppController from '../controller/controller';
+import { AppView } from '../view/appView';
+import { INewsData } from '../../types/index';
+import Sources from '../view/sources/sources';
+
+
+class App {
+    controller: AppController;
+    view: AppView;
+    constructor() {
+        this.controller = new AppController();
+        this.view = new AppView();
+    }
+
+    start(): void {
+        const tempSources = document.querySelector('.sources') as HTMLElement;
+        tempSources.addEventListener('click', (e: Event) => this.controller.getNews(e, (data: INewsData) => this.view.drawNews(data)));
+        this.controller.getSources((data: { sources: Array<ISourcesData> }) => this.view.drawSources(data));
+    }
+}
+
+export default App;
