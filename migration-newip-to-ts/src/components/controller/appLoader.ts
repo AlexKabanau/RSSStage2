d0f5bc7ae283fba0fa0dc91181e28b47559d18a2
Alexandr Kabanau <Azraill1989@gmail.com>
2022-12-10 18:53:27 +0300

    fix: change url

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.up.railway.app/', {
             apiKey: '4b02543cc901470dbbcdec480d959a7c', // получите свой ключ https://newsapi.org/
         });
     }
